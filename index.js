2bc82cacd238c31d932767a5dd4ac998f7ef4083
Leonardo Petruncko <62786185+leonardoPetruncko@users.noreply.github.com>
2022-12-18 18:57:07 -0300

    Adicionando arquitetura do sistema, refatorando código e criando rota

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const express = require('express')
+const express = require("express")
+const userRoute = require("./src/routes/user.route")
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+app.use("/soma", userRoute)
+
+// ROTA
+
+    // METHOD HTTP - CRUD -- (CREATE, READ, UPDATE, DELETE)
+    //GET - Pega Info
+    //POST - cria uma info
+    //PUT - Altera toda a info
+    //PATH - Altera parte da Info
+    //DELETE - Apaga uma info
+
+    //NAME
+    // Function (CallBack)
+
+// app.get('/', (req, res) => {
+  
+// })
 
 app.listen(3000)
